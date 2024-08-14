a3e1f11907e221ce6ae81beff42b53fbe2405cf8
Jacek <kontakt@jk-design.pl>
2024-08-14 03:44:47 +0200

    Pliki w edytorze

--- resources/views/form-elements/tintmce.blade.php
+++ resources/views/form-elements/tintmce.blade.php
@@ -7,10 +7,13 @@
         content_css: 'default',
         branding: false,
         height: 400,
-        plugins: "searchreplace autolink directionality visualblocks visualchars fullscreen image link gallery media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern filemanager code",
-        toolbar1: "formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat | gallery | code",
+        plugins: "searchreplace autolink directionality visualblocks visualchars fullscreen image link gallery media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern responsivefilemanager code",
+        toolbar1: "formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat | gallery | responsivefilemanager | code",
         relative_urls: false,
         image_advtab: true,
+        external_filemanager_path:"/js/editor/plugins/filemanager/",
+        filemanager_title:"kCMS Filemanager" ,
+        external_plugins: { "filemanager" : "{{ asset('/js/editor/plugins/filemanager/plugin.min.js') }}"},
         content_style:
             "@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');"
     });
