7866d8c111f79d42a577179c5ab195c78fa16d97
Jacek Kurowski <jacek@4dl.pl>
2025-12-06 13:57:20 +0100

    [Jacek][New] RWD

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -175,7 +175,7 @@
 <div id="contactBoxes">
     <div class="container">
         <div class="row">
-            <div class="col-4">
+            <div class="col-6 col-md-4">
                 <div class="contact-box">
                     <div class="contact-box-circle"><i class="las la-envelope-open-text"></i></div>
                     <h3>@lang('cms.napiszdonas-header')</h3>
@@ -183,7 +183,7 @@
                     <a href="mailto:espa@espa.com.pl" class="bttn bttn-sm mt-4">@lang('cms.wyslijwiadomosc-button')</a>
                 </div>
             </div>
-            <div class="col-4">
+            <div class="col-12 col-sm-6 col-md-4 mt-5 mt-sm-0">
                 <div class="contact-box">
                     <div class="contact-box-circle"><i class="las la-phone-alt"></i></div>
                     <h3>@lang('cms.zadzwondonas-header')</h3>
@@ -191,7 +191,7 @@
                     <a href="tel:+48943120867" class="bttn bttn-sm mt-4">@lang('cms.zadzwon-button')</a>
                 </div>
             </div>
-            <div class="col-4">
+            <div class="col-12 col-sm-6 col-md-4 mt-5 mt-md-0">
                 <div class="contact-box">
                     <div class="contact-box-circle"><i class="las la-map-marked-alt"></i></div>
                     <h3>@lang('cms.odwiedznas-header')</h3>
@@ -206,15 +206,15 @@
 <div id="contact">
     <div class="container">
         <div class="row">
-            <div class="col-6">
+            <div class="col-12 col-lg-6 order-3 order-lg-1">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2344.9413592026117!2d15.991904516328036!3d54.003816432960335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47004b1543d97ef5%3A0x881e9b091b6e2573!2sP.P.U.%20%22eSPa%22%20Pawe%C5%82%20Szumski!5e0!3m2!1spl!2spl!4v1657457330630!5m2!1spl!2spl" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-            <div class="col-3 d-flex align-items-center">
+            <div class="col-12 col-sm-6 col-lg-3 d-flex align-items-center">
                 <div class="contact-text">
                     {!! parse_text($contact->content) !!}
                 </div>
             </div>
-            <div class="col-3">
+            <div class="col-12 col-sm-6 col-lg-3 d-none d-sm-block">
                 <div class="w-100 h-100 object-fit">
                     <img src="{{ asset('/images/contact.jpg') }}" alt="P.P.U. eSPa">
                 </div>
